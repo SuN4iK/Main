f81415bbdf8ccc56b1195e29563d776ed9738a96
SuN4iK <egorka10.08.10@gmail.com>
2025-02-11 20:11:02 -0700

    Описал мейн по словам кота

---
+++ src/game/Main.java
@@ -0,0 +1,34 @@
+package game;
+
+import java.util.Scanner;
+
+public class Main {
+    int players;
+    for(;;){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество игроков: ");
+            if (players > 0) {
+                return(players);
+                System.out.println("Введите имя первого игрока");
+            } else {
+                if (players ==0){
+
+                    System.out.println("Игра прекращена.");
+                    break;
+
+                } else if (players<0){
+
+                    System.out.println("Число игроков не может быть меньше 0");
+
+                } else{
+
+                    System.out.println("Слишком много игроков!");
+                }
+            }
+
+        } else{
+            System.out.println("Вы ввели не число, или Ваше число слишком большое!");
+
+        }
+    }
+}
